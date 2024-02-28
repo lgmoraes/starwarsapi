13dc8559f539b4c75fc85586382945cf019a1127
Louis Moraes <lmoraes@hotmail.fr>
2024-02-28 16:45:10 +0100

    feat: Join film data to character

--- controllers/people.js
+++ controllers/people.js
@@ -2,11 +2,26 @@ const axios = require("axios");
 
 exports.getAllPeople = async (req, res, next) => {
   try {
-    const response = await axios.get("https://swapi.dev/api/people/");
-    const simplifiedCharacters = response.data.results.map(
-      ({ name, height, gender, films }) => ({ name, height, gender, films })
-    );
-    res.send(simplifiedCharacters);
+    Promise.all([
+      axios.get("https://swapi.dev/api/people/"),
+      axios.get("https://swapi.dev/api/films/"),
+    ]).then((responses) => {
+      const filmsList = responses[1].data.results;
+
+      const simplifiedCharacters = responses[0].data.results.map(
+        ({ name, height, gender, films }) => ({ name, height, gender, films })
+      );
+
+      simplifiedCharacters.forEach((char) => {
+        const episodes = char.films.map((film) => film.split("/")[5]);
+        const titles = episodes.map(
+          (ep) => filmsList.find((f) => f.episode_id == ep).title
+        );
+        char.films = titles;
+      });
+
+      res.send(simplifiedCharacters);
+    });
   } catch (error) {
     console.error("Erreur lors de la récupération des données : ", error);
     res.status(500).send("Erreur lors de la récupération des données");
@@ -15,14 +30,25 @@ exports.getAllPeople = async (req, res, next) => {
 
 exports.getPeople = async (req, res, next) => {
   try {
-    const response = await axios.get(
-      `https://swapi.dev/api/people/${req.params.id}/`
-    );
-    res.send({
-      name: response.data.name,
-      height: response.data.height,
-      gender: response.data.gender,
-      films: response.data.films,
+    Promise.all([
+      axios.get(`https://swapi.dev/api/people/${req.params.id}`),
+      axios.get("https://swapi.dev/api/films/"),
+    ]).then((responses) => {
+      const char = responses[0].data;
+      const filmsList = responses[1].data.results;
+
+      const episodes = char.films.map((film) => film.split("/")[5]);
+      const titles = episodes.map(
+        (ep) => filmsList.find((f) => f.episode_id == ep).title
+      );
+      char.films = titles;
+
+      res.send({
+        name: char.name,
+        height: char.height,
+        gender: char.gender,
+        films: char.films,
+      });
     });
   } catch (error) {
     console.error("Erreur lors de la récupération des données : ", error);
